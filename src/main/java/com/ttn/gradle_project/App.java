e14dcb5a0ac7c1c9a8c79b4bde2dda389b74e31c
MauryaNitin <nitin.maurya@tothenew.com>
2019-03-06 17:55:57 +0530

    Added gradle assignment

---
+++ src/main/java/com/ttn/gradle_project/App.java
@@ -0,0 +1,10 @@
+package com.ttn.gradle_project;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App{
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println(StringUtils.reverse(str));
+    }
+}
---
+++ src/main/java2/com/ttn/gradle_project/NewApp.java
@@ -0,0 +1,7 @@
+package com.ttn.gradle_project;
+
+public class NewApp {
+    public static void main(String[] args) {
+        System.out.println("New File Created!");
+    }
+}
